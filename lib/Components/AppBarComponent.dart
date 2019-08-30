65999d9c16675514810d35f2fe4356ff14cbd0ae
Mohammad Rahmani <rto1680@gmail.com>
2019-08-30 18:42:01 +0430

    Add custome action in appbar

--- lib/Components/AppBarComponent.dart
+++ lib/Components/AppBarComponent.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_accounting/Styles/TextStyles.dart';
 
 class AppBarComponent {
-  static getAppBar(title, leading) {
+  static getAppBar(title, leading, actions) {
     return AppBar(
       title: Text(title, style: TextStyles.appBarStyle,),
       actions: <Widget>[
-        Icon(null),
-        Icon(null),
+        actions
       ],
       leading: leading,
       backgroundColor: Colors.transparent,
