e7a10ad73adc61e14aeec9c37a85161a5a63df82
Mohammad Rahmani <rto1680@gmail.com>
2019-09-13 14:03:29 +0430

    Change the cardListComponent

--- lib/Components/CardListComponent.dart
+++ lib/Components/CardListComponent.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_accounting/Components/CardViewComponent.dart';
+import 'package:flutter_accounting/Database/moor_database.dart';
 import 'package:flutter_accounting/Models/CardModel.dart';
+import 'package:provider/provider.dart';
 
 class CardListComponent extends StatelessWidget {
-  final List<CardModel> cards;
+  final Stream<List<BankCard>> cards;
 
   CardListComponent({@required this.cards});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: getChildrenCards(),
-    );
+    return  getChildrenCards(context);
   }
 
-  List<Widget> getChildrenCards() {
-    return cards.map((card) => CardViewComponent(card)).toList();
+  // List<Widget> getChildrenCards() {
+  //   return cards.map((card) => CardViewComponent(card)).toList();
+  // }
+
+  StreamBuilder<List<BankCard>> getChildrenCards(BuildContext context) {
+    final database = Provider.of<AppDatabase>(context);
+    return StreamBuilder(
+      stream: database.getAllCards(),
+      builder: (context, AsyncSnapshot<List<BankCard>> snapshot) {
+        final tasks = snapshot.data ?? List();
+
+        return ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (_, index) {
+            final itemTask = tasks[index];
+            return CardViewComponent(itemTask);
+          },
+        );
+      },
+    );
   }
 }
